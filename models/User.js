a188c36377f88c19212fda09c5b69a44806d152d
Seb <samm1497@hotmail.com>
2022-11-15 18:48:14 +0000

    Models

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const validator = require('validator');
+const { validate } = require('./Thought');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate:{
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email',
+                isAsync: false
+            }
+        },
+        thoughts:[
+            {
+                type: Schema.Types.ObjectId,
+                ref:'Thought'
+            }
+        ],
+        friends:[ this ]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    }
+);
+
+//Virtual for friend count
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
