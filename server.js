a6950e6f6ddf9e0a362cfc2fd203abf882948ba1
Seb <samm1497@hotmail.com>
2022-11-15 20:17:27 +0000

    folder structure correction

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const PORT = process.env.PORT || 3001
 
 //express Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// app.use(require('./routes'))
 
 //Mongo connect
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mongo-social',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  );
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dbSocialData',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+  
 
   app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
